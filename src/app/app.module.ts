550679ca1a373f2757c100e14175e8b472f0ff61
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-15 12:39:16 -0400

    site-header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatologComponent } from './catolog/catolog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CatologComponent,
+    SiteHeaderComponent,
   ],
   imports: [
     BrowserModule
