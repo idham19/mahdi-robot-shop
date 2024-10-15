7a6f242dd96ad64bda6a4ac5ce013120e2776769
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-15 14:24:05 -0400

    html template added to the catalog.component.ts

--- src/app/catolog/catolog.component.ts
+++ src/app/catolog/catolog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catolog',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./catolog.component.css']
 })
 export class CatologComponent {
- 
+ product:IProduct;
+ constructor(){
+  this.product={
+    id:2,
+    description:"a friendly robot head",
+    name:"Friendly bot",
+    imageName:"head-friendly.png",
+    category:"Heads",
+    price:945.0,
+    discount:0.2
+  }
+ }
 }
