cb007155576cdce48bb01c0b733d936fb92927b9
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-17 09:50:00 -0400

    api-server and product.service added

--- api-server/index.js
+++ api-server/index.js
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
-import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+const express = require("express");
+const bodyParser = require("body-parser");
 
-@Component({
-  selector: 'bot-catolog',
-  templateUrl: './catolog.component.html',
-  styleUrls: ['./catolog.component.css']
-})
-export class CatologComponent {
- products:IProduct[];
- filter:string='';
-//  private cartSvc:CartService = inject(CartService)
+const app = express();
+app.use(bodyParser.json());
+/* 
+  IMPORTANT:
+    ***NEVER*** store credentials unencrypted like this.
+    This is for demo purposes only in order to simulate a functioning API serverr.
+*/
+const users = {
+  "jim@joesrobotshop.com": {
+    firstName: "Jim",
+    lastName: "Cooper",
+    email: "jim@joesrobotshop.com",
+    password: "very-secret",
+  },
+  "joe@joesrobotshop.com": {
+    firstName: "Joe",
+    lastName: "Eames",
+    email: "joe@joesrobotshop.com",
+    password: "super-secret",
+  },
+};
+let cart = [];
 
- constructor(private cartSvc:CartService){
-  this.products= [
+// use this to add a 1 second delay to all requests
+// app.use(function (req, res, next) {
+//   setTimeout(next, 1000);
+// });
+
+app.get("/api/products", (req, res) => {
+  let products = [
     {
       id: 1,
       description:
@@ -92,7 +109,7 @@ export class CatologComponent {
       price: 285,
       discount: 0,
     },
-  
+
     {
       id: 4,
       description: "A simple single-eyed head -- simple and inexpensive.",
@@ -188,14 +205,51 @@ export class CatologComponent {
       discount: 0,
     },
   ];
- }
+  res.send(products);
+});
 
- getFiltredProducts(){
-  return this.filter===''
-  ? this.products
-  :this.products.filter((product)=>product.category===this.filter)
- }
- addToCart(product:IProduct){
-  this.cartSvc.add(product)
- }
-}
+app.post("/api/cart", (req, res) => {
+  cart = req.body;
+  setTimeout(() => res.status(201).send(), 20);
+});
+
+app.get("/api/cart", (req, res) => res.send(cart));
+
+app.post("/api/register", (req, res) =>
+  setTimeout(() => {
+    const user = req.body;
+    if (user.firstName && user.lastName && user.email && user.password) {
+      users[user.email] = user;
+      res.status(201).send({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      });
+    } else {
+      res.status(500).send("Invalid user info");
+    }
+  }, 800)
+);
+
+/* IMPORTANT:
+    The code below is for demo purposes only and does not represent good security
+    practices. In a production application user credentials would be cryptographically 
+    stored in a database server and the password should NEVER be stored as plain text. 
+*/
+app.post("/api/sign-in", (req, res) => {
+  const user = users[req.body.email];
+  if (user && user.password === req.body.password) {
+    res.status(200).send({
+      userId: user.userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    });
+  } else {
+    res.status(401).send("Invalid user credentials.");
+  }
+});
+
+app.listen(8081, () =>
+  console.log("API Server listening on port 8081! http://localhost:8081/")
+);
